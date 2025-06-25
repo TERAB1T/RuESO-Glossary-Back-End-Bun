249b01a6fe51269095cd256bcb4a2c60a00ddda1
Alex Kiselyov <akiselyov@ukr.net>
2025-06-25 23:24:19 +0400

    Add Arena & Daggerfall

--- glossary/constants.ts
+++ glossary/constants.ts
@@ -2,4 +2,4 @@ export const DB_PATH = './db/glossary.db';
 
 export const TABLE_NAME = 'glossary';
 export const COLUMNS = ['game', 'type', 'en', 'ru'];
-export const VALID_GAMES = ['eso', 'skyrim', 'oblivion', 'morrowind', 'legends', 'blades', 'castles', 'redguard', 'battlespire', 'travels'];
+export const VALID_GAMES = ['eso', 'skyrim', 'oblivion', 'morrowind', 'legends', 'blades', 'castles', 'redguard', 'battlespire', 'travels', 'arena', 'daggerfall'];
